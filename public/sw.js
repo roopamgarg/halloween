a646fea50b0066dab3dd23d4b296d83f516cd658
roopamgarg <roopamg777@gmail.com>
2020-11-05 15:59:31 +0530

    added service worker

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,33 @@
-const CACHE_DYNAMIC_NAME = "dynamic-v1";
+// const CACHE_DYNAMIC_NAME = "dynamic-v1";
 
-self.addEventListener("install", (event) => {
-  console.log("installing");
-});
+// self.addEventListener("install", (event) => {
+//   console.log("installing");
+// });
 
-self.addEventListener("activate", (event) => {
-  console.log("activating");
-  return self.clients.cliam();
-});
+// self.addEventListener("activate", (event) => {
+//   console.log("activating");
+//   return self.clients.cliam();
+// });
 
-self.addEventListener("fetch", (event) => {
-  console.log(event.request);
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      if (res) {
-        return res;
-      }
-      fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
+// self.addEventListener("fetch", (event) => {
+//   console.log(event.request);
+//   event.respondWith(
+//     caches.match(event.request).then((res) => {
+//       if (res) {
+//         return res;
+//       }
+//       fetch(event.request).then((response) => {
+//         if (!response || response.status !== 200 || response.type !== "basic") {
+//           return response;
+//         }
 
-        const responseToCache = response.clone();
+//         const responseToCache = response.clone();
 
-        caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-          return response;
-        });
-      });
-    })
-  );
-});
+//         caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+//           cache.put(event.request, responseToCache);
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
