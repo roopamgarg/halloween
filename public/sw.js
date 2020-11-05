a03eb457d2299311ca661448860c4d8e7138d8f9
roopamgarg <roopamg777@gmail.com>
2020-11-05 15:21:00 +0530

    added service worker

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_DYNAMIC_NAME = "dynamic-v1";
+
+self.addEventListener("install", (event) => {
+  console.log("installing");
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("activating");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+          return response;
+        });
+      });
+    })
+  );
+});
